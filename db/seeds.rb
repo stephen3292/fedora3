1598c9a724a739bfa1615f87f84c876b9a567a19
App Academy Student <appacademy@Apps-Mac-mini-161.local>
2016-02-05 14:10:03 -0500

    heroku prep

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,62 +12,63 @@ Answer.destroy_all
 QuestionTag.destroy_all
 QuestionTagging.destroy_all
 
-  a = User.create(username: 'Bob', password: 'password',
+  a = User.create(username: 'Bob', password: 'password', avatar: "https://static-s.aa-cdn.net/img/ios/649977132/4effa0b29c7ad5f0dccb3f4d00980d36?v=1",
         description: "One of those people who calls himself an 'educator'")
   b = User.create(username: 'Tim', password: 'password',
         description: 'Professor of Some Obscure Subject')
-  c = User.create(username: 'Jim', password: 'password',
+  c = User.create(username: 'Jim', password: 'password', avatar: "http://www.brushwiz.com/images/nationalities/spanish/top_spanish_paintings_portrait_of_pope_innocent_x_by_diego_velazquez.jpg",
         description: "I like to answer other people's questions.")
   d = User.create(username: "Donald", password: "Donald",
-        description: "That's The to you")
+        description: "That's The Donald to you")
   e = User.create(username: "Guest User", password: "Guest User",
         description: "A gentleman and a scholar")
-  f = User.create(username: 'Tom', password: 'password',
-        description: 'ok')
-  g = User.create(username: 'Hillary', password: 'password',
+  f = User.create(username: 'Tom', password: 'password', avatar: "https://s-media-cache-ak0.pinimg.com/236x/11/f2/bc/11f2bc0eeddd2988f2c159b04ec2bb71.jpg",
+        description: 'Comes from Yahoo answers')
+  g = User.create(username: 'Anonymous', password: 'password', avatar: "http://mostfamousperson.net/LeonardoDiCaprio.png",
         description: 'better not say anything bad here')
-  h = User.create(username: 'Donald', password: 'password',
+  h = User.create(username: 'Donald', password: 'password', avatar: "http://rlv.zcache.com.au/thomas_gainsborough_the_blue_boy_square_sticker-r1a347ca5aa6b4f568ddff8f23508e554_v9wf3_8byvr_324.jpg",
         description: 'MD-PhD')
-  i = User.create(username: "Ted", password: "password",
+  i = User.create(username: "Ted", password: "password", avatar: "http://images5.fanpop.com/image/photos/31400000/Turtle-turtles-31450325-1525-1470.jpg",
         description: "Donald")
-  j = User.create(username: "Bill", password: "password",
-        description: "")
+  j = User.create(username: "Bill", password: "password", avatar: "http://www.umoga.com/images/user/SagyDemn/AGothic.png",
+        description: "a Few credentials with Arbitrary capitalization")
 
-  q0 = Question.create(user_id: c.id, title: 'Why is the Sign Out button so tiny?',
+  q0 = Question.create(user_id: c.id, title: 'Why is the Sign Out button gone?',
   body: 'idk', username: c.username)
   q1 = Question.create(user_id: a.id, title: 'Does the answer feature work yet?',
   body: 'idk', username: a.username)
-  q2 = Question.create(user_id: b.id, title: 'Why am I waiting two hours for pizza in Bushwick?',
+  q2 = Question.create(user_id: b.id, title: 'Why am I waiting two hours for pizza in Bushwick?', image: "http://farm8.static.flickr.com/7128/7666282294_3c4eb6a6de_m.jpg",
   body: 'dec', username: b.username)
   q3 = Question.create(user_id: c.id, title: 'Anyone think Heroku will screw this all up?',
   body: 'idk', username: c.username)
-  q4 = Question.create(user_id: d.id, title: 'Does the answer feature work yet?',
+  q4 = Question.create(user_id: a.id, title: 'Does the answer feature work yet?',
   body: 'idk', username: a.username)
   q5 = Question.create(user_id: e.id, title: 'Should I prevent myself from answering my own questions?',
-  body: 'dec', username: b.username)
+  body: 'dec', username: e.username)
   q6 = Question.create(user_id: c.id, title: '[yawns]',
   body: 'idk', username: c.username)
 
   q7 = Question.create(user_id: f.id, title: 'Is it a good idea to add more questions?',
-  body: 'idk', username: c.username)
+  body: 'idk', username: f.username)
   q8 = Question.create(user_id: a.id, title: 'Does the answer feature work yet?',
   body: 'idk', username: a.username)
   q9 = Question.create(user_id: b.id, title: 'Can I get dinner somewhere else?',
   body: 'dec', username: b.username)
   q10 = Question.create(user_id: h.id, title: 'I need to ask a new question right??',
-  body: 'idk', username: c.username)
-  q11 = Question.create(user_id: d.id, title: 'Was The Revenant any good?',
-  body: 'idk', username: a.username)
-  q12 = Question.create(user_id: j.id, title: 'How many questions should the page show?',
-  body: 'dec', username: b.username)
+  body: 'idk', username: h.username)
+  q11 = Question.create(user_id: d.id, title: 'Was The Revenant any good?', image: "http://www.clipartsheep.com/images/129/winnie-the-pooh-pic-14-129527.jpeg",
+  body: 'idk', username: d.username)
+  q12 = Question.create(user_id: j.id, title: 'How many questions should this page show?',
+  body: 'I really want to know!', username: j.username)
   q13 = Question.create(user_id: c.id, title: 'Should I write longer questions?',
   body: 'Sometimes I think question sites should have more space for the writer to complicate matters and for their question to make using the site more difficult for casual readers. A good way to do this might be to have an input space after the question title, maybe to write a question body. Does anybody think this is a good idea?', username: c.username)
-
+  q14 = Question.create(user_id: f.id, title: 'How those sprout things that hipster carry around work? Do they put them in their drinks or their food?',
+  body: 'bonus if you know how they taste', username: f.username)
 
 
   a1 = Answer.create(title: "Yeah, I just didn't want to answer you the first time.",
   body: "", user_id: a.id, question_id: q1.id, username: a.username)
-  a2 = Answer.create(title: 'I honestly have no clue. Also I think it would be a good idea to test and see how long an answer I can make before the formatting on the actual site gets a little bit strange. So far I have only written short answers in my seed data but now I will try something a little bit longer. THis was it will look more like the actual quora site, but honestly who knows how it will all go. I certainly dont.',
+  a2 = Answer.create(title: 'I honestly have no clue.',
   body: "", user_id: d.id, question_id: q2.id, username: d.username)
   a3 = Answer.create(title: "I don't think it's a problem, you can do it on the real site.",
   body: "", user_id: e.id, question_id: q5.id, username: e.username)
@@ -75,21 +76,17 @@ QuestionTagging.destroy_all
   body: "", user_id: c.id, question_id: q1.id, username: c.username)
   a5 = Answer.create(title: "Probably because it doesn't work yet.",
   body: "", user_id: d.id, question_id: q0.id, username: d.username)
-  a6 = Answer.create(title: "Be nice, the real site doesn't have one there either and it works if you refresh.",
+  a6 = Answer.create(title: "The real site doesn't have one!",
   body: "", user_id: a.id, question_id: q0.id, username: a.username)
 
   a7 = Answer.create(title: "No.",
   body: "", user_id: a.id, question_id: q13.id, username: a.username)
-  a8 = Answer.create(title: 'I honestly have no clue. Also I think it would be a good idea to test and see how long an answer I can make before the formatting on the actual site gets a little bit strange. So far I have only written short answers in my seed data but now I will try something a little bit longer. THis was it will look more like the actual quora site, but honestly who knows how it will all go. I certainly dont.',
-  body: "", user_id: d.id, question_id: q2.id, username: d.username)
-  a9 = Answer.create(title: "Doesn't that question need a picture?",
-  body: "", user_id: g.id, question_id: q11.id, username: g.username)
+  a8 = Answer.create(title: "That's a good question. But I think a space for answers is more important. Sometimes people like to write really really long answers on this site and reading through them can be quite tedious. I doubt this answer will be as long as some of the other ones but it's already getting up there in length.",
+  body: "", user_id: d.id, question_id: q7.id, username: d.username)
+  a9 = Answer.create(title: "Doesn't that question need a picture?", image: "http://vignette2.wikia.nocookie.net/villains/images/0/04/QuestionMark.png/revision/latest?cb=20080722190045",
+  body: "", user_id: c.id, question_id: q3.id, username: c.username)
   a10 = Answer.create(title: 'It was ok, mostly grunting.',
-  body: "", user_id: c.id, question_id: q1.id, username: c.username)
-  a11 = Answer.create(title: "Probably because it doesn't work yet.",
-  body: "", user_id: d.id, question_id: q0.id, username: d.username)
-  a12 = Answer.create(title: "Be nice, the real site doesn't have one there either and it works if you refresh.",
-  body: "", user_id: a.id, question_id: q0.id, username: a.username)
+  body: "", user_id: g.id, question_id: q11.id, username: g.username)
 
 
 
@@ -102,15 +99,3 @@ QuestionTagging.destroy_all
 
   qt3 = QuestionTag.create(name: "pizza")
   qtg3 = QuestionTagging.create(question_id: q2.id, question_tag_id: qt3.id)
-
-  qt4 = QuestionTag.create(name: "tech")
-  qtg4 = QuestionTagging.create(question_id: q0.id, question_tag_id: qt4.id)
-
-  qt5 = QuestionTag.create(name: "tech")
-  qtg5 = QuestionTagging.create(question_id: q0.id, question_tag_id: qt5.id)
-
-  qt6 = QuestionTag.create(name: "tech")
-  qtg6 = QuestionTagging.create(question_id: q0.id, question_tag_id: qt6.id)
-
-  qt7 = QuestionTag.create(name: "tech")
-  qtg7 = QuestionTagging.create(question_id: q0.id, question_tag_id: qt7.id)
